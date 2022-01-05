c3fc6c3abfb124986f4bd2553d0df5f91c69ddac
diyorbek57 <58900685+diyorbek57@users.noreply.github.com>
2022-01-05 20:33:33 +0500

    //

--- app/src/main/java/com/b12/lesson/MainActivity.java
+++ app/src/main/java/com/b12/lesson/MainActivity.java
@@ -1,6 +1,10 @@
 package com.b12.lesson;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
@@ -11,15 +15,20 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
 public class MainActivity extends AppCompatActivity {
 
+    private static final int PICK_PHOTO_FOR_AVATAR = 1;
     ImageView imageView;
     boolean doubleBackToExitPressedOnce = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        imageView = findViewById(R.id.imageView);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE);
 
 
@@ -36,6 +45,10 @@ public class MainActivity extends AppCompatActivity {
         button1.setOnClickListener(v -> {
             Intent intent = new Intent(MainActivity.this, SendEmail.class);
             startActivity(intent);
+        });
+        Button button2 = findViewById(R.id.button3);
+        button2.setOnClickListener(v -> {
+            pickImage();
         });
 
     }
@@ -60,5 +73,31 @@ public class MainActivity extends AppCompatActivity {
         }, 2000);
     }
 
+    public void pickImage() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_PHOTO_FOR_AVATAR);
+    }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICK_PHOTO_FOR_AVATAR && resultCode == Activity.RESULT_OK) {
+            if (data == null) {
+                return;
+            }
+            try {
+                final Uri imageUri = data.getData();
+                InputStream inputStream = getContentResolver().openInputStream(imageUri);
+                final Bitmap selectedImage = BitmapFactory.decodeStream(inputStream);
+                imageView.setImageBitmap(selectedImage);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+
+
+        }
+    }
 }
